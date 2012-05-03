1fcd8eac5534c2a12ea5f86153369508cc7f9448
Michiel <code@m01.eu>
2012-05-03 22:45:49 +0100

    Added exception for numbers that are out of range and objects now keep track of what's on the display

--- 7seg.py
+++ 7seg.py
@@ -1,6 +1,21 @@
 import RPi.GPIO as GPIO
 
-class seven_segment_display():
+class NumberOutOfRange(Exception):
+  """
+  This exception is thrown for numbers that are outside the range of values that a
+  7 segment display can display.
+  """
+
+  def __init__(self, number):
+    self.number = number
+
+  def __str__(self):
+    return repr("The number %d is outside the range of your 7 segment display" % self.number)
+
+  def __unicode__(self):
+    return u"The number %d is outside the range of your 7 segment display" % self.number
+
+class SevenSegmentDisplay():
   segments = {'a': 11, 'b': 12, 'c': 13, 'd': 15, 'e': 16, 'f': 18, 'g': 22 }
 
   numbers = [
@@ -16,6 +31,8 @@ class seven_segment_display():
     "abcdfg", #9
     ]
 
+  current_state = None #.. which means off
+
   # setup
   def __init__(self):
     for s in self.segments:
@@ -27,10 +44,19 @@ class seven_segment_display():
     for s in self.segments:
       GPIO.output(self.segments[s], True)
 
+    #update state
+    self.current_state = None
+
   def set(self, number_to_display):
     """ show some number on the 7 segment display. """
+
+    if not (0 <= number_to_display <= 9):
+      raise NumberOutOfRange(number_to_display)
+
     self.off()
     segments_to_turn_on = self.numbers[number_to_display]
     for s in segments_to_turn_on:
       GPIO.output(self.segments[s], False)
 
+    #update state
+    self.current_state = number_to_display
